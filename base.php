52c501c04de39f7582f8f6f8a44603a0b24aa8ce
PhilipStubbs <24510543+PhilipStubbs@users.noreply.github.com>
2018-10-22 16:54:35 +0200

    fix for base

--- base.php
+++ base.php
@@ -3,6 +3,6 @@
 	if ($actual_link == "localhost:8080")
 		echo '<base href="http://localhost:8080/">';
 	if ($actual_link == "philipstubbs.co.za")
-		echo '<base href="philipstubbs.co.za/"> ';
+		echo '<base href="https://philipstubbs.co.za/"> ';
 
 ?>
