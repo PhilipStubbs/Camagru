e0e1696b8ec6da1b378680be1520b0eddc7f64d2
PhilipStubbs <24510543+PhilipStubbs@users.noreply.github.com>
2018-10-24 08:52:32 +0200

    ext type added

--- Content/upload_image_code.php
+++ Content/upload_image_code.php
@@ -8,8 +8,6 @@ if (isset($_POST['uploadsubmit']))
 	{
 		$file = $_FILES['file'];
 
-		// print_r($file);
-
 		$fileName = $file['name'];
 		$fileTmpName = $file['tmp_name'];
 		$fileSize = $file['size'];
@@ -19,41 +17,56 @@ if (isset($_POST['uploadsubmit']))
 		$fileExt = explode('.', $fileName);
 		$fileActualExt = strtolower(end($fileExt));
 
+		$sizeCheck = getimagesize($fileTmpName);
+
 		$allowed = array('jpg', 'jpeg', 'png');
-		
 		if (in_array($fileActualExt, $allowed))
 		{
-			echo "3";
-			if ($fileError === 0)
+			if ($sizeCheck)
 			{
-				if ($fileSize < 500000)
+				echo "3";
+				if ($fileError === 0)
 				{
-					$fileNameNew = uniqid('', true).".".$fileActualExt;
-					
+					if ($fileSize < 5000000)
+					{
+						$fileNameNew = uniqid('', true).".".$fileActualExt;
+						
 
-					$fileDest = 'upload_test/'.$fileNameNew;
-					move_uploaded_file($fileTmpName, $fileDest);
-					header("Location: /Content/upload_post.php");
+						$fileDest = 'upload_test/'.$fileNameNew;
+						// move_uploaded_file($fileTmpName, $fileDest);
+						$imagedata = file_get_contents($fileTmpName);
+			
+						$base64 = base64_encode($imagedata);
+						$_SESSION['tmp_image'] = $base64;
+						header("Location: /Content/upload_post.php");
+					}
+					else
+					{
+						$_SESSION['error'] = "Your File is too big, 5mb max";
+						header("Location: /Content/upload_image.php?Size_fail");
+					}
 				}
 				else
 				{
-					$_SESSION['error'] = "Your File is too big, 5mb max";
-					header("Location: /Content/upload_image.php?Size_fail");
+					$_SESSION['error'] = "Error Uploading";
+					header("Location: /Content/upload_image.php?Upload_fail");
 				}
 			}
-			else
+			else 
 			{
-				$_SESSION['error'] = "Error Uploading";
-				header("Location: /Content/upload_image.php?Upload_fail");
+				$_SESSION['error'] = "Conflicting ext types";
+				header("Location: /Content/upload_image.php?Ext_fail");
 			}
 		}
 		else
 		{
-			$_SESSION['error'] = "$fileActualExt is not allowed!
+			$_SESSION['error'] = "$fileActualExt is not allowed! <br />
 			jpg, jpeg or png ONLY";
 			header("Location: /Content/upload_image.php?Ext_fail");
 		}
 	}
+		
+	
 	else
 	{
 		$_SESSION['error'] = "Require a file!";
