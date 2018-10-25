22c618033b31dc533ddaa63328a198c4d792c23e
PhilipStubbs <24510543+PhilipStubbs@users.noreply.github.com>
2018-10-25 10:40:11 +0200

    fix for base

--- base.php
+++ base.php
@@ -2,10 +2,15 @@
 	$actual_link = "$_SERVER[HTTP_HOST]";
 	$type = $_SERVER['SERVER_PROTOCOL'];
 	
-	if (substr($type,0, 5) == "HTTP/")
-		$full_link = "http://".$actual_link;
-	else if (substr($type,0, 5) == "HTTPS")
+	// if (substr($type,0, 5) == "HTTP/")
+	// 	$full_link = "http://".$actual_link;
+	// else if (substr($type,0, 5) == "HTTPS")
+	// 	$full_link = "https://".$actual_link;
+	if ($actual_link == "philipstubbs.co.za")
 		$full_link = "https://".$actual_link;
+	else
+		$full_link = "http://".$actual_link;
+
 	$base = "<base href=" . "\"".$full_link . "\">" ;
 	echo ($base);
 	
